6ae51d4bb164a288fb227def0885b8ca97a6477d
KABEO-PC\81903 <g.okabe.business@gmail.com>
2020-08-20 14:50:37 +0900

    プロジェクト ファイルを追加します。

---
+++ MVC App/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_App.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            ViewData["msg"] = "Hello!World! Let's leran MVC";
+
+            return View();
+        }
+    }
+}
---
+++ MVC App/Models/Member.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App.Models
+{
+    public class Member
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public  int Id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 時間
+        /// </summary>
+        public DateTime dt { get; set; }
+    }
+}
